c74635599f98e923924e9cd9b971d1e00273bf92
rubybrah <rubybrah@gmail.com>
2013-04-29 01:00:27 +0200

    refactoring

--- cypher.rb
+++ cypher.rb
@@ -1,14 +1,14 @@
 def cypher(an_array)
 
-  na = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  ca = na.rotate(2)
+  normal_array = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  cypher_array = normal_array.rotate(2)
   awesome_cyphered_array = []
 
   an_array.each do |element|
     0.upto(25) do |i|
       case element
-        when na[i]
-          awesome_cyphered_array << ca[i]
+        when normal_array[i]
+          awesome_cyphered_array << cypher_array[i]
       end
     end
   end
